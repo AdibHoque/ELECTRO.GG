02f2c4263dd40aa87aa0efa1a22438e1233fa540
Adib Hoque <adibhoque04@gmail.com>
2020-08-18 18:00:02 +0600

    Add files via upload

---
+++ mega.js
@@ -0,0 +1,361 @@
+"mega": {
+"id": "15_Mega",
+"names": {
+"english":"Mega Beedrill",
+"japanese":"メガスピアー",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:65,atk:150,def:40,satk:15,sdef:80,spd:145}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/015_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},
+"mega": {
+"id": "18_Mega",
+"names": {
+"english":"Mega Pidgeot",
+"japanese":"メガピジョット",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:83,atk:80,def:80,satk:135,sdef:80,spd:121}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/018_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},
+"mega": {
+"id": "80_Mega",
+"names": {
+"english":"Mega Slowbro",
+"japanese":"メガヤドラン",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:95,atk:75,def:180,satk:130,sdef:80,spd:30}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/80_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+}, 
+"mega": {
+"id": "208_Mega",
+"names": {
+"english":"Mega Steelix",
+"japanese":"メガハガネール",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:75,atk:125,def:230,satk:55,sdef:95,spd:30}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/208_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+}, 
+"mega": {
+"id": "254_Mega",
+"names": {
+"english":"Mega Sceptile",
+"japanese":"メガジュカイン",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:70,atk:110,def:75,satk:145,sdef:85,spd:145}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/254_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},  
+"mega": {
+"id": "260_Mega",
+"names": {
+"english":"Mega Swampert",
+"japanese":"メガラグラージ",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:100,atk:150,def:110,satk:95,sdef:110,spd:70}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/260_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},  
+"mega": {
+"id": "302_Mega",
+"names": {
+"english":"Mega Sableye",
+"japanese":"メガヤミラミ",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:50,atk:85,def:125,satk:85,sdef:115,spd:20}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/302_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},  
+"mega": {
+"id": "319_Mega",
+"names": {
+"english":"Mega Sharpedo",
+"japanese":"メガサメハダー",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:70,atk:140,def:70,satk:110,sdef:65,spd:105}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/319_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},  
+"mega": {
+"id": "323_Mega",
+"names": {
+"english":"Mega Camerupt",
+"japanese":"メガバクーダ",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:70,atk:120,def:100,satk:145,sdef:105,spd:20}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/323_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},  
+"mega": {
+"id": "334_Mega",
+"names": {
+"english":"Mega Altaria",
+"japanese":"メガチルタリス",
+"chinese":"",
+"french":""
+},
+"type":[""],
+"base":{hp:75,atk:110,def:110,satk:110,sdef:105,spd:80}
+"image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/334_f2.png",
+"altnames":[],
+"before":[],
+"after":[],
+"evovleAt":"",
+"shiny":""
+},  	
+
+#362	
+Mega Glalie
+メガオニゴーリ		
+80	120	 80	 120	80	100	
+
+#373	
+Mega Salamence
+メガボーマンダ		 	
+95	145	 130	120	 90 	120	
+
+#376	
+Mega Metagross
+メガメタグロス	
+80 145 150	105 	110	 110	
+
+#380	
+Mega Latias
+メガラティアス
+80	100	 120	140	 150	110	
+
+#381	
+Mega Latios
+メガラティオス
+80	130	 100	160	 120	110	
+
+#384	
+Mega Rayquaza
+メガレックウザ		 	
+105 	180	 100	180	 100	115	
+
+#428	
+Mega Lopunny
+メガミミロップ		 	
+65	136	 94	 54 	96	135	
+
+#475	
+Mega Gallade
+メガエルレイド		 	
+68	165	95	65	115	110	
+
+#531	
+Mega Audino
+メガタブンネ		 	
+103	60	126	80	126	50	
+
+#719	
+Mega Diancie
+メガディアンシー		 	
+50	160	110	160	110	110	
+
+#003	
+Mega Venusaur
+メガフシギバナ		 	
+80	100	123	122	120	80	
+
+#006	
+Mega Charizard X
+メガリザードンX		 	
+78	130	111	130	85	100	
+
+#006	
+Mega Charizard Y
+メガリザードンY		 	
+78	104	78	159	115	100	
+
+#009	
+Mega Blastoise
+メガカメックス			
+79	103	120	135	115	78	
+
+#065	
+Mega Alakazam
+メガフーディン			
+55	50	65	175	95	150	
+
+#094	
+Mega Gengar
+メガゲンガー		 	
+60	65	80	170	95	130	
+
+#115	
+Mega Kangaskhan
+メガガルーラ			
+105	125	100	60	100	100	
+
+#127	
+Mega Pinsir
+メガカイロス		 	
+65	155	120	65	90	105	
+
+#130	
+Mega Gyarados
+メガギャラドス		 	
+95	155	109	70	130	81	
+
+#142	
+Mega Aerodactyl
+メガプテラ		 	
+80	135	85	70	95	150	
+
+#150	
+Mega Mewtwo X
+メガミュウツーX		 	
+106	190	100	154	100	130	
+
+#150	
+Mega Mewtwo Y
+メガミュウツーY	
+106	150	70	194	120	140	
+
+#181	
+Mega Ampharos
+メガデンリュウ		 	
+90	95	105	165	110	45	
+
+#212
+Mega Scizor
+メガハッサム		 	
+70	150	140	65	100	75	
+
+#214	
+Mega Heracross
+メガヘラクロス		 	
+80	185	115	40	105	75	
+
+#229	
+Mega Houndoom
+メガヘルガー		 	
+75	90	90	140	90	115	
+
+#248	
+Mega Tyranitar
+メガバンギラス		 	
+100	164	150	95	120	71	
+
+#257	
+Mega Blaziken
+メガバシャーモ		 	
+80	160	80	130	80	100	
+
+#282	
+Mega Gardevoir
+メガサーナイト		 	
+68	85	65	165	135	100	
+
+#303	
+Mega Mawile
+メガクチート		 	
+50	105	125	55	95	50	
+
+#306	
+Mega Aggron
+メガボスゴドラ			
+70	140	230	60	80	50	
+
+#308	
+Mega Medicham
+メガチャーレム		 	
+60	100	85	80	85	100	
+
+#310	
+Mega Manectric
+メガライボルト			
+70	75	80	135	80	135	
+
+#354	
+Mega Banette
+メガジュペッタ			
+64	165	75	93	83	75	
+
+
+#359	
+Mega Absol
+メガアブソル
+65	150	60	115	60	115	
+
+#445	
+Mega Garchomp
+メガガブリアス
+108	170	115	120	95	92	
+
+#448	
+Mega Lucario
+メガルカリオ	
+70	145	88	140	70	112	
+
+#460	
+Mega Abomasnow
+メガユキノオー	 	
+90 132	105	132	105	30
